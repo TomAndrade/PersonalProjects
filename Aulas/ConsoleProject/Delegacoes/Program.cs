f290b2ba14a3d43d36be612615543779b64c62c9
Well Andrade <wtonsouzaandrade@gmail.com>
2025-10-21 16:24:26 -0300

    add: delegates

---
+++ Aulas/ConsoleProject/Delegacoes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacoes
+{
+    internal class Program
+    {
+        public delegate double MinhasDelegacoes(double x, double y);
+        public static double ExemploSoma(double x, double y){ double retorno = x + y; Console.WriteLine("Somando..."+retorno); return retorno;}
+        public static double ExemploMulti(double x, double y)
+        { 
+            double retorno = x * y; 
+            Console.WriteLine("Multiplicando..." + retorno); 
+            return retorno;
+        }
+        public static double ExemploSubtrai(double x, double y){ double retorno = x - y; Console.WriteLine("Subtraindo..."+retorno); return retorno;}
+        public static double ExemploDivide(double x, double y){ double retorno = x / y; Console.WriteLine("Dividindo..."+retorno); return retorno;}
+
+        // Um método que recebe como parâmetro um delegate...
+        public static void ExecutaOperacao(MinhasDelegacoes delegacoes)
+        {
+            delegacoes(50,10);
+        }
+
+        static void Main(string[] args)
+        {
+            MinhasDelegacoes operacoes;
+            operacoes = ExemploSoma;
+            operacoes += ExemploSubtrai;
+            operacoes += ExemploMulti;
+            operacoes += ExemploDivide;
+            double resultadoDelegado = operacoes(100,210);
+            Console.WriteLine("Exemplo de delegação para vários métodos : "+ resultadoDelegado);
+
+            ExecutaOperacao(ExemploMulti);
+        }
+    }
+}
