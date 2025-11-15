48eceabc49a4ddbe63ee2730b42a7462868ee8c1
Well Andrade <wtonsouzaandrade@gmail.com>
2025-11-15 17:55:06 -0300

    add: maskedTextBox examples

---
+++ Aulas/ConsoleProject/SobreMaskedTextBox/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SobreMaskedTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            maskedTextBoxNum = new MaskedTextBox();
+            maskedTextBoxLetras = new MaskedTextBox();
+            maskedTextBoxDate = new MaskedTextBox();
+            mTBPlacaDeCarro = new MaskedTextBox();
+            maskedTextBoxHora = new MaskedTextBox();
+            maskedTextBoxDecimais = new MaskedTextBox();
+            maskedTextBoxSenha = new MaskedTextBox();
+            maskedTextBoxCelular = new MaskedTextBox();
+            chkboxMercosul = new CheckBox();
+            buttonVerifica = new Button();
+            SuspendLayout();
+            // 
+            // maskedTextBoxNum
+            // 
+            maskedTextBoxNum.Location = new Point(12, 12);
+            maskedTextBoxNum.Name = "maskedTextBoxNum";
+            maskedTextBoxNum.Size = new Size(425, 23);
+            maskedTextBoxNum.TabIndex = 0;
+            // 
+            // maskedTextBoxLetras
+            // 
+            maskedTextBoxLetras.Location = new Point(12, 41);
+            maskedTextBoxLetras.Name = "maskedTextBoxLetras";
+            maskedTextBoxLetras.Size = new Size(425, 23);
+            maskedTextBoxLetras.TabIndex = 0;
+            // 
+            // maskedTextBoxDate
+            // 
+            maskedTextBoxDate.Location = new Point(12, 70);
+            maskedTextBoxDate.Name = "maskedTextBoxDate";
+            maskedTextBoxDate.Size = new Size(425, 23);
+            maskedTextBoxDate.TabIndex = 0;
+            // 
+            // mTBPlacaDeCarro
+            // 
+            mTBPlacaDeCarro.Location = new Point(12, 99);
+            mTBPlacaDeCarro.Name = "mTBPlacaDeCarro";
+            mTBPlacaDeCarro.Size = new Size(425, 23);
+            mTBPlacaDeCarro.TabIndex = 0;
+            // 
+            // maskedTextBoxHora
+            // 
+            maskedTextBoxHora.Location = new Point(12, 128);
+            maskedTextBoxHora.Name = "maskedTextBoxHora";
+            maskedTextBoxHora.Size = new Size(425, 23);
+            maskedTextBoxHora.TabIndex = 0;
+            // 
+            // maskedTextBoxDecimais
+            // 
+            maskedTextBoxDecimais.Location = new Point(12, 157);
+            maskedTextBoxDecimais.Name = "maskedTextBoxDecimais";
+            maskedTextBoxDecimais.Size = new Size(425, 23);
+            maskedTextBoxDecimais.TabIndex = 0;
+            // 
+            // maskedTextBoxSenha
+            // 
+            maskedTextBoxSenha.Location = new Point(12, 186);
+            maskedTextBoxSenha.Name = "maskedTextBoxSenha";
+            maskedTextBoxSenha.Size = new Size(425, 23);
+            maskedTextBoxSenha.TabIndex = 0;
+            // 
+            // maskedTextBoxCelular
+            // 
+            maskedTextBoxCelular.Location = new Point(12, 215);
+            maskedTextBoxCelular.Name = "maskedTextBoxCelular";
+            maskedTextBoxCelular.Size = new Size(425, 23);
+            maskedTextBoxCelular.TabIndex = 0;
+            // 
+            // chkboxMercosul
+            // 
+            chkboxMercosul.AutoSize = true;
+            chkboxMercosul.Location = new Point(455, 102);
+            chkboxMercosul.Name = "chkboxMercosul";
+            chkboxMercosul.Size = new Size(163, 19);
+            chkboxMercosul.TabIndex = 1;
+            chkboxMercosul.Text = "Modelo Antigo (BRA2019)";
+            chkboxMercosul.UseVisualStyleBackColor = true;
+            chkboxMercosul.Click += chkboxMercosul_Click;
+            // 
+            // buttonVerifica
+            // 
+            buttonVerifica.Font = new Font("Segoe UI", 12F);
+            buttonVerifica.Location = new Point(355, 257);
+            buttonVerifica.Name = "buttonVerifica";
+            buttonVerifica.Size = new Size(82, 30);
+            buttonVerifica.TabIndex = 2;
+            buttonVerifica.Text = "Validar";
+            buttonVerifica.UseVisualStyleBackColor = true;
+            buttonVerifica.Click += buttonVerifica_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonVerifica);
+            Controls.Add(chkboxMercosul);
+            Controls.Add(maskedTextBoxCelular);
+            Controls.Add(maskedTextBoxSenha);
+            Controls.Add(maskedTextBoxDecimais);
+            Controls.Add(maskedTextBoxHora);
+            Controls.Add(mTBPlacaDeCarro);
+            Controls.Add(maskedTextBoxDate);
+            Controls.Add(maskedTextBoxLetras);
+            Controls.Add(maskedTextBoxNum);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MaskedTextBox maskedTextBoxNum;
+        private MaskedTextBox maskedTextBoxLetras;
+        private MaskedTextBox maskedTextBoxDate;
+        private MaskedTextBox mTBPlacaDeCarro;
+        private MaskedTextBox maskedTextBoxHora;
+        private MaskedTextBox maskedTextBoxDecimais;
+        private MaskedTextBox maskedTextBoxSenha;
+        private MaskedTextBox maskedTextBoxCelular;
+        private CheckBox chkboxMercosul;
+        private Button buttonVerifica;
+    }
+}
---
+++ Aulas/ConsoleProject/SobreMaskedTextBox/Form1.cs
@@ -0,0 +1,63 @@
+namespace SobreMaskedTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Use MaskTextBox para adicionar máscaras em campos de texto a fim de padroniza-los.
+            // Há máscaras predefinidas nas propriedades do controle sem que precise digitar tudo aqui diretamente.
+            // Neste caso, inserir # é opcional. Inserir 0 é obrigatório.
+            maskedTextBoxNum.Mask = "0000";
+
+            // No caso de letras, usa-se L (obrigatório) e ? (opcional).
+            maskedTextBoxLetras.Mask = "LLLLLLLL";
+
+            // Exemplo de uso para datas:
+            maskedTextBoxDate.Mask = "00/00/0000";
+
+            // Exemplo de uso para placas de carro:
+            mTBPlacaDeCarro.Mask = "LLL0L00";
+
+            // Exemplo de uso para horas:
+            maskedTextBoxHora.Mask = "00:00:##";
+
+            // Exemplo de uso para números decimais - use o ponto ao invés da vírgula para marcar:
+            maskedTextBoxDecimais.Mask = "000.00000";
+
+            // Para senhas - use o PasswordChar para substituir todos os caracteres por aquele escolhido, escondendo a senha:
+            maskedTextBoxSenha.Mask = "0000";
+            maskedTextBoxSenha.PasswordChar = '*';
+
+            // Exemplo de uso para número de celular - use \\ para inserir caracteres forçados ou que não podem ser deletados:
+            maskedTextBoxCelular.Mask = "(\\000) \\90000-0000";
+
+        }
+
+        private void chkboxMercosul_Click(object sender, EventArgs e)
+        {
+            if (chkboxMercosul.Checked)
+            {
+                mTBPlacaDeCarro.Mask = "LLL-0000";
+            }
+            else
+            {
+                mTBPlacaDeCarro.Mask = "LLL0L00";
+            }
+        }
+
+        private void buttonVerifica_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is MaskedTextBox m && !m.MaskCompleted)
+                {
+                    MessageBox.Show("Existem campos obrigatórios não preenchidos.","Atenção",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            MessageBox.Show("Todos os campos foram preenchidos!");
+        }
+    }
+}
