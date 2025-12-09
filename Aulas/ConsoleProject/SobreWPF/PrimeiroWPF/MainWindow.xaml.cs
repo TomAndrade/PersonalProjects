ff039613057050b3ee61aa386f69619ded2d04d0
Well Andrade <wtonsouzaandrade@gmail.com>
2025-12-09 15:26:57 -0300

    update: add grids and buttons

--- Aulas/ConsoleProject/SobreWPF/PrimeiroWPF/MainWindow.xaml.cs
+++ Aulas/ConsoleProject/SobreWPF/PrimeiroWPF/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace PrimeiroWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pass1.Visibility = Visibility.Collapsed;
+        }
     }
 }
