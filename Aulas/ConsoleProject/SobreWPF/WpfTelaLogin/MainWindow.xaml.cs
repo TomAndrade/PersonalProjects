74b094d371daefc7a3a79a1e06933e0384349997
Well Andrade <wtonsouzaandrade@gmail.com>
2025-12-11 17:27:04 -0300

    add: new login plus window WPF

---
+++ Aulas/ConsoleProject/SobreWPF/WpfTelaLogin/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTelaLogin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxUser.Text == "well")
+            {
+                if (passboxPass.Password == "1234")
+                {
+                    MessageBox.Show("Login succeed!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Acesso acesso = new Acesso();
+                    acesso.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login failed: incorrect password","Failed",MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Login failed: incorrect user","Failed",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
